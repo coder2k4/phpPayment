89fe6891a72a3fe8d5d45cc6d6e573420bdec3e4
Standalone <coder2k@mail.ru>
2020-09-06 14:45:05 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+});
+
+$('.buy').click(function () {
+    var price = $(this).data('price'), product = $(this).data('product');
+    $('#price').val(price);
+    $('#product').val(product);
+    $('#cart').modal();
+    return false;
+});
